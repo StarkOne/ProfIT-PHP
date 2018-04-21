c19565e38ae25cf803ef03b99314f43f33edbc43
StarkOne <Stark.pro40@gmail.com>
2018-04-21 20:11:51 +0700

    class user

--- index.php
+++ index.php
@@ -2,7 +2,11 @@
 
 require __DIR__ . '/autoload.php';
 
-$db = new \App\Db();
+//$db = new \App\Db();
 //$res = $db->execute('CREATE TABLE foo (id SERIAL)');
-$data = $db->query('SELECT * FROM foo');
-var_dump($data);
+//$data = $db->query('SELECT * FROM users', 'App\Models\User');
+//$user = new \App\Models\User();
+
+// путь нужен обязательно app\models\
+$users = \App\Models\User::findAll();
+var_dump($users);
